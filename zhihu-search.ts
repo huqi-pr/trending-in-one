d2b5391b46536222f2642aa5d2b0606c6bb44e68
huqi <huqi1008301@163.com>
2022-03-10 13:53:42 +0800

    Update zhihu-search.ts

--- zhihu-search.ts
+++ zhihu-search.ts
@@ -33,7 +33,7 @@ if (await exists(fullPath)) {
 const wordsAll = mergeWords(words, wordsAlreadyDownload);
 
 export const zhihuSearchData = wordsAll.map((x) => {
-  x.url = `https://www.zhihu.com/search?q=${x.query}`;
+  x.url = `https://www.zhihu.com/search?q=${x.query.replace(/(^\s+)|(\s+$)|\s+/g,'%20')}`;
   return x;
 });
 
